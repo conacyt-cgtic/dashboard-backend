bda79af23c6c7a22d42a8d50b32e85e0cc8c5fa0
garellano <garellano@192.168.100.7>
2019-02-04 23:20:18 -0600

    refactor for test classes

--- src/test/java/mx/conacyt/cgtic/dashboard/service/Calculatortest.java
+++ src/test/java/mx/conacyt/cgtic/dashboard/service/Calculatortest.java
@@ -1,19 +1,16 @@
-package mx.conacyt.cgtic.dashboard.config;
+package mx.conacyt.cgtic.dashboard.service;
+
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
-
-import mx.conacyt.cgtic.dashboard.service.CustomerService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CalculatorTest {
+public class Calculatortest {
     @Autowired
     private CustomerService service;
     
