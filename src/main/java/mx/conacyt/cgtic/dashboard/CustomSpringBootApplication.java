4a0f0d72f39fc266272a93cede617cd7ad029d2b
garellano <garellano@mbp-de-gustavo>
2019-02-16 14:03:29 -0600

    commit 3 final

--- src/main/java/mx/conacyt/cgtic/dashboard/CustomSpringBootApplication.java
+++ src/main/java/mx/conacyt/cgtic/dashboard/CustomSpringBootApplication.java
@@ -9,6 +9,7 @@ public class CustomSpringBootApplication {
 	public static void main(String[] args) {
 	    System.out.println("hola mundo 1"); 
         System.out.println("hola mundo 2"); 
+        System.out.println("hola mundo 3"); 
 		SpringApplication.run(CustomSpringBootApplication.class, args);
 	}
 }
